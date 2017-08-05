2606862bfedb32176120885fea8e24d4ad89fb62
Peter Ivanov <ivanovp@gmail.com>
2017-08-05 08:50:12 +0200

    Missig include file was added.

---
+++ Inc/pifs_dir.h
@@ -0,0 +1,24 @@
+/**
+ * @file        pifs_dir.h
+ * @brief       Internal header of Pi file system
+ * @author      Copyright (C) Peter Ivanov, 2017
+ *
+ * Created:     2017-06-11 09:10:19
+ * Last modify: 2017-06-27 19:35:38 ivanovp {Time-stamp}
+ * Licence:     GPL
+ */
+#ifndef _INCLUDE_PIFS_DIR_H_
+#define _INCLUDE_PIFS_DIR_H_
+
+#include <stdint.h>
+
+#include "common.h"
+#include "flash.h"
+#include "pifs_config.h"
+#include "pifs.h"
+
+typedef pifs_status_t (*pifs_dir_walker_func_t)(pifs_dirent_t * a_dirent, void * a_fund_data);
+pifs_status_t pifs_walk_dir(pifs_char_t * a_path, bool_t a_recursive, bool_t a_stop_at_error,
+                            pifs_dir_walker_func_t a_dir_walker_func, void * a_func_data);
+
+#endif /* _INCLUDE_PIFS_DIR_H_ */
