160af9ec33d0b26484b9ee227945a5df24e8aec0
Peter Ivanov <ivanovp@gmail.com>
2017-08-19 08:34:32 +0200

    License and readme were added.

--- source/test/flash_test.h
+++ source/test/flash_test.h
@@ -6,6 +6,19 @@
  * Created:     2017-06-11 09:10:19
  * Last modify: 2017-06-27 19:35:38 ivanovp {Time-stamp}
  * Licence:     GPL
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _INCLUDE_FLASH_TEST_H_
 #define _INCLUDE_FLASH_TEST_H_
